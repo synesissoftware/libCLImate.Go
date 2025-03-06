95697f17fa0c2a06a08c35037e39266431319591
Matt Wilson <matthew.wilson.synesis@gmail.com>
2025-03-07 06:34:07 +1100

    ~ improved testing

--- abort_test.go
+++ abort_test.go
@@ -1,6 +1,7 @@
 package libclimate_test
 
 import (
+	"github.com/stretchr/testify/require"
 	libclimate "github.com/synesissoftware/libCLImate.Go"
 	"github.com/synesissoftware/libCLImate.Go/internal"
 
@@ -14,8 +15,10 @@ func Test_Abort_1(t *testing.T) {
 
 	climate, _ := libclimate.Init(func(cl *libclimate.Climate) (err error) {
 
+		cl.ProgramName = "myapp"
+
 		return nil
-	})
+	}, libclimate.InitFlag_PanicOnFailure)
 
 	stm := new(bytes.Buffer)
 	exiter := new(internal.CaptureExiter)
@@ -23,38 +26,32 @@ func Test_Abort_1(t *testing.T) {
 	msg := "Some-failure-condition"
 	err := errors.New("SOME-FAILURE-REASON")
 
-	climate.ProgramName = "myapp"
 	climate.Abort(msg, err, stm, exiter)
 
 	actual := stm.String()
 	expected := fmt.Sprintf("myapp: %s: %s\n", msg, err)
 
-	if expected != actual {
-
-		t.Errorf("expected '%v' != actual '%v'", expected, actual)
-	}
+	require.Equal(t, expected, actual)
 }
 
 func Test_Abort_2(t *testing.T) {
 
 	climate, _ := libclimate.Init(func(cl *libclimate.Climate) (err error) {
 
+		cl.ProgramName = "myapp"
+
 		return nil
-	})
+	}, libclimate.InitFlag_PanicOnFailure)
 
 	stm := new(bytes.Buffer)
 	exiter := new(internal.CaptureExiter)
 
 	msg := "Some-failure-condition"
 
-	climate.ProgramName = "myapp"
 	climate.Abort(msg, nil, stm, exiter)
 
 	actual := stm.String()
 	expected := fmt.Sprintf("myapp: %s\n", msg)
 
-	if expected != actual {
-
-		t.Errorf("expected '%v' != actual '%v'", expected, actual)
-	}
+	require.Equal(t, expected, actual)
 }
